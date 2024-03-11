b4e82b1004ff69bd5c3694d4a66f645c580f4046
Yash <yashghogre100@gmail.com>
2024-03-11 15:32:10 +0530

    Made pages for organizing committee section

--- src/app/committee/organizing-committee/organizing-team/page.js
+++ src/app/committee/organizing-committee/organizing-team/page.js
@@ -1,10 +1,10 @@
-import ConveyerCard from "../components/ConveyerCard"
+import ConveyerCard from "@/app/components/ConveyerCard"
 
 const page = () => {
-    return (
-        <div className="overflow-x-hidden">
+  return (
+    <div className="overflow-x-hidden">
             <div className='flex justify-center items-center py-5'>
-                <h1 className='font-black text-2xl'>Conveyers</h1>
+                <h1 className='font-black text-2xl'>Organizing Team</h1>
             </div>
             <div className='flex w-screen justify-evenly items-center my-5'>
                 <ConveyerCard img={'/images/rakhimamimg.jpeg'} name={'Rakhi D. Wajgi'} des={'HOD, Computer Technology'} />
