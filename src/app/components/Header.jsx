fb1b353eeb54e5bc8dceefb585ee41381053b8a2
Pranav <pranavsuryavanshi910@gmail.com>
2024-03-11 15:01:42 +0530

    added accordions

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,7 +6,7 @@ import icasetLogo from "../icaset_logo.jpg";
 const Header = () => {
   return (
     <div className="bg-[#31363F] text-white w-full m-0 py-[1rem] px-[3rem] flex justify-between items-center">
-      <Image src={icasetLogo} alt="icaset_Logo" className="w-[120px]" />
+      <Image src={icasetLogo} alt="icaset_Logo" className="w-[120px]"/>
       <h1 className="text-3xl text-center mx-[10rem] my-[1rem]">
         International Conference on Application of Science, Engineering and
         Application
