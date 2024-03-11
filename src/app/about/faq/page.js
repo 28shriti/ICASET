d01d29e91498e6cf7af88aafcca888c194f52d5f
Pranav <pranavsuryavanshi910@gmail.com>
2024-03-11 14:18:32 +0530

    added accordians

--- src/app/about/faq/page.js
+++ src/app/about/faq/page.js
@@ -1,3 +1,30 @@
+// 'use client'
+import { accordionData } from "@/app/data"
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
+
 export default function Attend() {
-    return <p>Attend</p>
+    return <Accordion type="single" collapsible >
+        {/* <h1></h1> */}
+        {
+            accordionData.map((val, index) => {
+                return (
+                    <AccordionItem value={`item-${index}`}>
+                        <AccordionTrigger>{val.question}</AccordionTrigger>
+                        <AccordionContent>
+                            {val.answer}
+                        </AccordionContent>
+                    </AccordionItem>
+                )
+            })
+        }
+
+    </Accordion>
 }
--- src/app/data.js
+++ src/app/data.js
@@ -2,4 +2,6 @@ const data = {
     notice: "This is the notice in marquee tag",
     conferenceDate: "4 May 2024"
 }
+
+export const accordionData = [{question : "question ", answer : "answer"}, {question : "question ", answer : "answer"}, {question : "question ", answer : "answer"}, {question : "question ", answer : "answer"}]
 export default data;
