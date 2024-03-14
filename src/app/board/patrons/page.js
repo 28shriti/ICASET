d78c442fc0b80f4147566db12927f3c2136b3f8a
Pranav <pranavsuryavanshi910@gmail.com>
2024-03-14 15:09:43 +0530

    update commit

--- src/app/board/patrons/page.js
+++ src/app/board/patrons/page.js
@@ -1,28 +1,19 @@
 import React from 'react'
 import Image from "next/image"
 import { patrons } from "@/app/data"
+import BoardMember from '@/app/components/BoardMember'
 
 function page() {
     return (
-        <div className='py-20'>
-            <div className="flex-column mx-auto w-4/12  rounded-lg py-3 self-center bg-[#31363F] bg-opacity-60 h-[70vh] overflow-y-auto no-scrollbar">
-                <h1 className="text-white text-center text-4xl font-extrabold">Patrons</h1>
-                {patrons.map((val, index) => {
+        <div className='py-20 backdrop-blur-lg bg-[#31363f]'>
+            <div className="flex-column mx-auto w-full rounded-lg px-10 py-4 self-center bg-opacity-60 h-[70vh] overflow-y-auto no-scrollbar">
+                <h1 className="text-white text-center text-4xl mb-10 font-extrabold">Patrons</h1>
+                <div className='flex flex-row flex-wrap justify-center gap-x-5 gap-y-5'>
+                    {patrons.map((val, index) => {
 
-                    return (<div key={index} className="text-white bg-[#222831] gap-5 py-3 transparent items-center flex flex-row m-10  rounded-full ">
-                        <div >
-                            <Image src={`/images/${val.image}`} width={70} height={70} className="mx-3 rounded-full" />
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <div >
-                                <p className="">{val.name}</p>
-                            </div>
-                            <div >
-                                <p className="font-light">{val.designation}</p>
-                            </div>
-                        </div>
-                    </div>)
-                })}
+                        return (<BoardMember img={val.image} name={val.name} designation={val.designation} link={"https://www.google.com"} />)
+                    })}
+                </div>
             </div>
         </div>
     )
