934e0ebed3128e172cfc3d5f562c8cbc480017ef
spectropaws <spectropaws777@gmail.com>
2024-03-14 16:08:12 +0000

    styled BoardMembers component

--- src/app/board/iab/page.js
+++ src/app/board/iab/page.js
@@ -15,8 +15,8 @@ const page = () => {
 
     return (
         <div className="overflow-x-hidden backdrop-blur-md backdrop-contrast-50">
-            <div>
-                <h1 className="title font-black text-4xl text-center pt-10 text-white">International Advisory Board</h1>
+            <div className="w-auto flex justify-center">
+                <h1 className="title font-black text-4xl text-center mt-10 py-3 px-5 text-white w-fit rounded-2xl bg-[#222831] bg-opacity-70">International Advisory Board</h1>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center w-screen p-10 gap-5">
                 {/* <Table className='w-[70vw] ml-[15vw] mt-10 mb-10 border bg-[#31363F] bg-opacity-70 text-white'>
--- src/app/board/nab/page.js
+++ src/app/board/nab/page.js
@@ -16,7 +16,7 @@ const page = () => {
     return (
         <div className="overflow-x-hidden backdrop-blur-md backdrop-contrast-50">
             <div>
-                <h1 className="title font-black text-4xl text-center pt-10 text-white">National Advisory Board</h1>
+                <h1 className="title font-black text-4xl text-center pt-10 text-[#222831]">National Advisory Board</h1>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center w-screen p-10">
                 {/*<Table className='w-[70vw] ml-[15vw] mt-10 mb-10 border bg-[#31363F] bg-opacity-70 text-white'>
