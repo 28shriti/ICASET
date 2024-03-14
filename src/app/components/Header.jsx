515417f41107ab608458eaabf66175133ecc603f
spectropaws <spectropaws777@gmail.com>
2024-03-14 16:46:26 +0000

    responsive header and keynote speakers

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,14 +7,14 @@ import { AppProgressBar } from "next-nprogress-bar";
 
 const Header = () => {
   return (
-    <div className="bg-[#31363F] text-white w-full m-0 py-[1rem] px-[3rem] flex justify-between items-center">
+    <div className="bg-[#31363F] text-white w-full m-0 p-3 lg:py-[1rem] lg:px-[3rem] flex justify-between items-center">
       <AppProgressBar color="white"/>
-      <Image src={icasetLogo} alt="icaset_Logo" className="w-[120px]" />
-      <h1 className="text-3xl font-bold text-center mx-[10rem] my-[1rem]">
+      <Image src={icasetLogo} alt="icaset_Logo" className="w-[120px] hidden md:block" />
+      <h1 className="text-3xl font-bold text-center md:mx-5 lg:mx-10 my-[1rem]">
         International Conference on Application of Science, Engineering and
         Technology - 2024
       </h1>
-      <Image src={'/images/ycceLogoNew.png'} alt="ycce_Logo" height={100} width={150}/>
+      <Image src={'/images/ycceLogoNew.png'} alt="ycce_Logo" className="hidden md:block" height={100} width={150}/>
     </div>
   );
 };
