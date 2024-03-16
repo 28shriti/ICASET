124579cb97e29f094c779a95a7271e9ca8cb0fbc
spectropaws <spectropaws777@gmail.com>
2024-03-16 11:23:16 +0000

    changed background images

---
+++ src/app/about/YCCE/page.js
@@ -0,0 +1,18 @@
+import { aboutYCCE } from "@/app/data"
+import Image from "next/image"
+export default function YCCE() {
+    return (
+        <>
+            <div className=" backdrop-blur-sm w-full p-10">
+                <div className=" bg-white p-10 opacity-80 mb-10 rounded-3xl">
+                    <h1 className=" flex justify-center font-bold pb-10 text-5xl text-[#333]">About YCCE</h1>
+                    <div className=" flex">
+                        <p >{aboutYCCE[0]}</p>
+                        <Image src="/images/background.jpg" alt="/" width={550} height={70} className=" flex -left-12 rounded-2xl" />
+                    </div>
+                    <p>{aboutYCCE[1]}</p>
+                </div>
+            </div>
+        </>
+    )
+}
