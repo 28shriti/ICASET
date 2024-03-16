af02c3e6857bb681910da827250c0679c294c7d3
28shriti <shritinandewar@gmail.com>
2024-03-16 11:15:40 +0530

    logo updated

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 import Image from "next/image";
-import icasetLogo from "../icaset_logo.jpg";
+// import icasetLogo from "../icaset_logo.png";
 import { AppProgressBar } from "next-nprogress-bar";
 
 const Header = () => {
   return (
     <div className="bg-[#31363F] text-white w-full m-0 p-3 lg:py-[1rem] lg:px-[3rem] flex justify-between items-center">
       <AppProgressBar color="white"/>
-      <Image src={icasetLogo} alt="icaset_Logo" className="w-[120px] hidden md:block" />
+      <Image src={'/images/icaset_logo.png'} width={130} height={130} alt="icaset_Logo" className=" hidden md:block mix-blend-screen" />
       <h1 className="text-3xl font-bold text-center md:mx-5 lg:mx-10 my-[1rem]">
         International Conference on Application of Science, Engineering and
         Technology - 2024
