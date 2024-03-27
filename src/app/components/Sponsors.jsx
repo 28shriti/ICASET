ba4c21a2433abadbe44229643c2632215c20bd19
Yash <yashghogre100@gmail.com>
2024-03-27 16:01:02 +0530

    rounded sponsors

--- src/app/components/Sponsors.jsx
+++ src/app/components/Sponsors.jsx
@@ -10,24 +10,24 @@ const Sponsors = () => {
                     <div className='flex flex-col gap-1'>
                         <p className='text-center font-medium'>Sponsors</p>
                         <div className='flex flex-col md:flex-row gap-10 items-center'>
-                            <Image src={'/images/sponsors/EliteGlobalPvtLtd.jpeg'} width={300} height={90} alt='sponsor' />
-                            <Image src={'/images/sponsors/JankiElectromac.jpeg'} width={100} height={100} alt='sponsor' />
+                            <Image src={'/images/sponsors/EliteGlobalPvtLtd.jpeg'} className='rounded-md' width={300} height={90} alt='sponsor' />
+                            <Image src={'/images/sponsors/JankiElectromac.jpeg'} className='rounded-md' width={100} height={100} alt='sponsor' />
                         </div>
                     </div>
                     <div className='flex items-center flex-col gap-2'>
                         <div>
                             <p className='text-center font-medium'>Media Partner</p>
                         </div>
-                        <Image src={'/images/sponsors/Lokmat.png'} width={200} height={100} alt='sponsor' />
+                        <Image src={'/images/sponsors/Lokmat.png'} className='rounded-md' width={200} height={100} alt='sponsor' />
                     </div>
                 </div>
                 <div className='flex flex-col gap-2 mt-10'>
                     <p className='text-center font-medium'>Technically Co-sponsored by</p>
                     <div className='flex flex-col md:flex-row gap-12 justify-center items-center'>
-                        <Image src={'/images/sponsors/YashEarthingPune.jpeg'} width={100} height={100} alt='sponsor' />
-                        <Image src={'/images/sponsors/SkywingsTechnology.png'} width={150} height={100} alt='sponsor' />
-                        <Image src={'/images/sponsors/6SimplexPvtLtd.jpg'} width={200} height={50} alt='sponsor' />
-                        <Image src={'/images/sponsors/CryptoForensicTechnology.jpg'} width={200} height={100} alt='sponsor' />
+                        <Image src={'/images/sponsors/YashEarthingPune.jpeg'} className='rounded-md' width={100} height={100} alt='sponsor' />
+                        <Image src={'/images/sponsors/SkywingsTechnology.png'} className='rounded-md' width={150} height={100} alt='sponsor' />
+                        <Image src={'/images/sponsors/6SimplexPvtLtd.jpg'} className='rounded-md' width={200} height={50} alt='sponsor' />
+                        <Image src={'/images/sponsors/CryptoForensicTechnology.jpg'} className='rounded-md' width={200} height={100} alt='sponsor' />
                     </div>
                 </div>
             </div>
