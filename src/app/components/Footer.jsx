51047495559ed24e715e1b81f3fd5e53b5a66c1e
Yash <yashghogre100@gmail.com>
2024-03-27 16:13:06 +0530

    responsive footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,31 +4,31 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-[#222831]  text-white p-20 pb-0 flex justify-center">
-        <div className="mr-[60px] w-[400px] h-[300px]" >
-            <Image src={'/images/icaset_logo.png'} width={300} height={150} alt="icaset_Logo" className=" hidden md:block mix-blend-screen object-cover object-center" />
-            <p style={{fontSize : "15px"}}>
-            International conference on Science Engineering & Technology(ICASET)
-            which will transpire on 23rd-24th November 2024,Nagpur,India</p>
-        </div>
-        <div className='m-[60px] w-[400px] h-[300px]' >
-            <h1 className='text-cyan p-[30px]' style={{fontSize : "30px",color :"cyan",padding :"30px"}}>Quick Links </h1>
-            <ul>
-      
-                <li>•<Link href="/" className="item"> Home </Link></li>
-                <li>•<Link href="/about/preamble" className="item"> About</Link></li>
-                <li>•<Link href="/dates" className="item"> Important Dates</Link></li>
-                <li>•<Link href="/schedule" className="item"> Programme Schedule</Link></li>
-            </ul>
-        </div>
-        <div className='m-[60px] w-[400px] h-[300px]' >
-        <h1 className='text-cyan p-[30px]' style={{fontSize : "30px",color :"cyan",padding :"30px"}}>Get in Touch </h1>
-            <ul>
-                <li>Phone no :<a href="tel:9322549802">+91-9999999999</a> </li>
-                <li>Email : @ycce.edu</li>
-                <li>YCCE Nagpur</li>
-            </ul>
-        </div>
+        <footer className="w-full bg-[#222831]  text-white md:p-20 py-5 flex flex-col md:flex-row justify-center items-center">
+            <div className=" m-auto md:mr-[60px] md:w-[400px] md:h-[300px] flex flex-col items-center" >
+                <Image src={'/images/icaset_logo.png'} width={300} height={150} alt="icaset_Logo" className=" md:block mix-blend-screen object-cover object-center" />
+                <p className='text-center text-md px-3'>
+                    International Conference on Application of Science, Engineering and
+                    Technology - 2024</p>
+            </div>
+            <div className='m-auto md:m-[60px] md:w-[400px] md:h-[300px]' >
+                <h1 className='text-cyan p-[30px]' style={{ fontSize: "30px", color: "cyan", padding: "30px" }}>Quick Links </h1>
+                <ul>
+
+                    <li>•<Link href="/" className="item"> Home </Link></li>
+                    <li>•<Link href="/about/preamble" className="item"> About</Link></li>
+                    <li>•<Link href="/dates" className="item"> Important Dates</Link></li>
+                    <li>•<Link href="/schedule" className="item"> Programme Schedule</Link></li>
+                </ul>
+            </div>
+            <div className='m-auto md:m-[60px] md:w-[400px] md:h-[300px]' >
+                <h1 className='text-cyan p-[30px]' style={{ fontSize: "30px", color: "cyan", padding: "30px" }}>Get in Touch </h1>
+                <ul>
+                    <li>Phone no :<a href="tel:9322549802">+91-9999999999</a> </li>
+                    <li>Email : @ycce.edu</li>
+                    <li>YCCE Nagpur</li>
+                </ul>
+            </div>
         </footer>
     );
 }
--- src/app/components/Sponsors.jsx
+++ src/app/components/Sponsors.jsx
@@ -8,7 +8,7 @@ const Sponsors = () => {
             <div className='flex flex-col gap-5'>
                 <div className='flex flex-col md:flex-row justify-evenly gap-5'>
                     <div className='flex flex-col gap-1'>
-                        <p className='text-center font-medium'>Sponsors</p>
+                        <p className='text-center text-xl font-medium'>Sponsors</p>
                         <div className='flex flex-col md:flex-row gap-10 items-center'>
                             <Image src={'/images/sponsors/EliteGlobalPvtLtd.jpeg'} className='rounded-md' width={300} height={90} alt='sponsor' />
                             <Image src={'/images/sponsors/JankiElectromac.jpeg'} className='rounded-md' width={100} height={100} alt='sponsor' />
@@ -16,13 +16,13 @@ const Sponsors = () => {
                     </div>
                     <div className='flex items-center flex-col gap-2'>
                         <div>
-                            <p className='text-center font-medium'>Media Partner</p>
+                            <p className='text-center font-medium text-xl'>Media Partner</p>
                         </div>
                         <Image src={'/images/sponsors/Lokmat.png'} className='rounded-md' width={200} height={100} alt='sponsor' />
                     </div>
                 </div>
                 <div className='flex flex-col gap-2 mt-10'>
-                    <p className='text-center font-medium'>Technically Co-sponsored by</p>
+                    <p className='text-center font-medium text-xl'>Technically Co-sponsored by</p>
                     <div className='flex flex-col md:flex-row gap-12 justify-center items-center'>
                         <Image src={'/images/sponsors/YashEarthingPune.jpeg'} className='rounded-md' width={100} height={100} alt='sponsor' />
                         <Image src={'/images/sponsors/SkywingsTechnology.png'} className='rounded-md' width={150} height={100} alt='sponsor' />
