e2b6e777a2996bf7e27998ce00afbeb0a5d300b4
Yash <yashghogre100@gmail.com>
2024-03-27 10:42:01 +0530

    adjusted sponsors

--- src/app/about/YCCE/page.js
+++ src/app/about/YCCE/page.js
@@ -6,7 +6,7 @@ export default function YCCE() {
             <div className=" backdrop-blur-sm w-full p-10">
                 <div className=" bg-white p-10 bg-opacity-80 mb-10 rounded-3xl">
                     <h1 className=" flex justify-center font-bold pb-10 text-5xl text-[#333]">About YCCE</h1>
-                    <div className=" flex">
+                    <div className="flex gap-5 pb-5">
                         <p >{aboutYCCE[0]}</p>
                         <Image src="/images/background.jpg" alt="/" width={550} height={70} className=" flex -left-12 rounded-2xl" />
                     </div>
