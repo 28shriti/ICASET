7b2cf5ae0f536769d0837012c63fc0e1f5fbbd3f
Yash <yashghogre100@gmail.com>
2024-03-28 12:37:24 +0530

    minor changes

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,7 +6,7 @@ const NotFound = () => {
         <div className='w-full h-[50vh] flex justify-center items-center bg-black bg-opacity-70'>
             <div className='flex flex-col gap-5'>
                 <h3 className='text-white font-semibold text-2xl'>Looks like you are lost!</h3>
-                <Link href={'/'} className='bg-white text-black rounded-md flex justify-center p-3'>Return to Homepage</Link>
+                <Link href={'/'} className='bg-white hover:bg-[#F9F6EE] text-black rounded-md flex justify-center p-3'>Return to Homepage</Link>
             </div>
         </div>
     )
