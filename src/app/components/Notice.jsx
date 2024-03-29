0b693c3613bd592e8752736f28482c12db53c5ec
Pranav <pranavsuryavanshi910@gmail.com>
2024-03-29 14:12:38 +0530

    updated accordian section

--- src/app/components/Notice.jsx
+++ src/app/components/Notice.jsx
@@ -1,7 +1,7 @@
 import data from "@/app/data"
 
 export default function Notice() {
-    return <marquee className="text-red-600 font-bold font-mono text-lg p-2 mb-0 bg-[#222831] bg-opacity-60 backdrop-blur-md">
+    return <marquee className="text-white font-bold font-mono text-lg p-2 mb-0 bg-[#222831] bg-opacity-60 backdrop-blur-md">
         <p>Notice: {data.notice}</p>
     </marquee>;
 }
