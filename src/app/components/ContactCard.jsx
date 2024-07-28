01580580000cabcac5979d18639a9453f2423365
Piyush22070 <piyushbhoyar6480@gmail.com>
2024-07-28 16:24:14 +0530

    Glitch Restored

--- src/app/components/ContactCard.jsx
+++ src/app/components/ContactCard.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 
 const ContactCard = (props) => {
   return (
-    <div className="bg-[#31363F] h-[300px] w-[200px] rounded-lg shadow-lg text-center  bg-opacity-80">
-      <h1 className="text-white text-xl mb-5">Contact Us:</h1>
-      <div className=" rounded-lg relative">
-        <div className="absolute top-0 w-full h-24 rounded-t-lg flex items-center justify-center">
-        <h2 className="text-white text-2xl font-bold mb-2">{props.name}</h2>
+    <div className="bg-[#31363F] h-[300px] w-[300px] rounded-lg shadow-lg text-center  bg-opacity-80">
+      <h1 className="text-white text-xl p-2 ">Contact Us:</h1>
+
+      <div className=" rounded-lg">
+        <div className="rounded-t-lg h-[180px] flex items-center justify-center">
+            <h2 className="text-white text-2xl font-bold ">Dr. {props.name}</h2>
         </div>
-        <div className="mt-24 text-left">
+        <div>
           <ol>
           <li className="text-white mb-1">{props.email}</li>
           <li className="text-white">{props.mobile}</li>
