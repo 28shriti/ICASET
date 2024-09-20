f44d5f45b09a58d37f95ee4b11e71a122112673e
Piyush22070 <piyushbhoyar6480@gmail.com>
2024-09-20 22:03:59 +0530

    registration page changes

--- src/app/registration/page.js
+++ src/app/registration/page.js
@@ -9,103 +9,97 @@ export const metadata = {
 export default function Registration() {
     return ( 
         <MainContainer>
-            <div className="overflow-x-hidden backdrop-blur-md backdrop-contrast-50 p-2 md:p-10">
-                <div className=" bg-white p-10 bg-opacity-80 lg:mb-10 rounded-3xl">
-                    <h1 className=" flex justify-center font-bold pb-10 text-2xl md:text-5xl text-center text-[#333]">
+            <div className="overflow-x-hidden backdrop-blur-md backdrop-contrast-50 p-2 md:p-10 text-white">
+                <div className=" bg-[#222831] p-10 bg-opacity-70 lg:mb-10 rounded-3xl">
+                    <h1 className=" flex justify-center font-bold pb-10 text-2xl md:text-5xl text-center">
                         Registration
                     </h1>
                     <p className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        The registration phase begins as soon as the paper has been accepted. One has to complete the following FOUR registration steps. The registered papers will be published by the Grenze Scientific Society.
+                    The registration phase begins as soon as the paper has been accepted. One has to complete the following FOUR registration steps. All the accepted, registered and presented papers will be published by 
+                    the <a href="https://mbox.theides.org/l/Umn5VjBSrxMpCQXVaqUNrA/C4nTgqQiSmFl5OvMRH2ZTw/a1INeKaDiDlV8ruK2149cg" className="underline font-bold"> Grenze Scientific Society</a>and it will be made available in the
+                    <a href="https://mbox.theides.org/l/Umn5VjBSrxMpCQXVaqUNrA/PE892y892QMyWTO8v2gNMZ3rDw/a1INeKaDiDlV8ruK2149cg" className="underline font-bold"> GRENZE International Journal of Engineering and Technology (GIJET),</a> and will be indexed in Scopus.
                     </p>
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <strong>Step 1 - Registration Fee </strong> <br />
+                        <strong className="text-xl">Step 1 - Registration Fee </strong> <br />
                         <p>
-                            It is mandatory for at least one author of an accepted paper to register in order for the paper to appear in the Proceedings and be included in the Technical Program. If an author has got more than one accepted papers, each paper has to be registered.
+                        It is mandatory for <strong>at least one author of an accepted paper to register in order for the paper to appear in the Proceedings and be included in the Technical Program</strong> If an author has got more than one accepted papers, each paper has to be registered.
                         </p>
                     </div>
                     <br/>
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <strong>
+                        <strong className="text-xl">
                             First Author Registration Fees
                         </strong> <br />
-                        <table className="border-2 border-black mt-2 w-full">
+                        <table className="border-black border-2 mt-2 w-full text-center">
                             <tbody>
                                 <tr>
-                                    <th className="border-2 border-black p-2">Paper Registration</th>
-                                    <th className="border-2 border-black p-2">Authors from Host Country</th>
-                                    <th className="border-2 border-black p-2">International Authors</th>
+                                    <th className="border-2 p-2">Paper Registration</th>
+                                    <th className="border-2 p-2">Authors from Host Country</th>
+                                    <th className="border-2 p-2">International Authors</th>
                                 </tr>
                                 <tr>
-                                    <td className="border-2 border-black p-2">Industry Delegate</td>
-                                    <td className="border-2 border-black p-2">INR 7500</td>
-                                    <td className="border-2 border-black p-2">USD 120</td>
+                                    <td className="border-2 p-2">Industry Delegate <br/> <p className="italic"> (Up to 5 pages)</p></td>
+                                    <td className="border-2 p-2">INR 7500</td>
+                                    <td className="border-2 p-2">USD 120</td>
                                 </tr>
                                 <tr>
-                                    <td className="border-2 border-black p-2">Academia </td>
-                                    <td className="border-2 border-black p-2">INR 6500</td>
-                                    <td className="border-2 border-black p-2">USD 100</td>
+                                    <td className="border-2 p-2">Academia  <br/> <p className="italic"> (Up to 5 pages)</p></td>
+                                    <td className="border-2 p-2">INR 6500</td>
+                                    <td className="border-2 p-2">USD 100</td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <p><strong>First Author Registration Fees</strong>{" "}
-                            is mandatory for all the papers.
-                        </p>
                     </div>
                     <br />
 
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <strong>Extra Charges (if applicable)</strong> <br />
+                        <strong className="text-xl">Extra Charges (if applicable)</strong> <br />
                             <table className="border-2 border-black my-2 w-full">
                                 <tbody>
                                     <tr>
-                                        <th className="border-2 border-black p-2">Item</th>
-                                        <th className="border-2 border-black p-2">Authors from Host country</th>
-                                        <th className="border-2 border-black p-2">International authors</th>
+                                        <th className="border-2 p-2">Item</th>
+                                        <th className="border-2 p-2 text-center">Authors from Host country</th>
+                                        <th className="border-2 p-2 text-center">International authors</th>
                                     </tr>
                                     <tr>
-                                        <td className="border-2 border-black p-2">Extra page charge (per Page)</td>
-                                        <td className="border-2 border-black p-2">INR 1000</td>
-                                        <td className="border-2 border-black p-2">USD 20</td>
+                                        <td className="border-2 p-2">Extra page charge (per Page)</td>
+                                        <td className="border-2 p-2 text-center">INR 1000</td>
+                                        <td className="border-2 p-2 text-center">USD 20</td>
                                     </tr>
                                 </tbody>
                             </table>
                     </div>
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <div><strong>Registration Fees - Co-Author or Attendee/Listener or Spouse intrested to participate inperson</strong><br />
+                        <div><strong className="text-xl">Registration Fees - Co-Author or Attendee or Listener or Spouse intrested to participate in person</strong><br />
 
                         <table className="border-2 border-black my-2 w-full">
                                 <tbody>
                                     <tr>
-                                        <th className="border-2 border-black p-2">Co-Author or Attendee/Listener or Spouse</th>
-                                        <th className="border-2 border-black p-2">Authors from Host Country</th>
-                                        <th className="border-2 border-black p-2">International Authors</th>
+                                        <th className="border-2 p-2" rowSpan="2">Co-Author or Attendee / Listener or Spouse</th>
+                                        <th className="border-2 p-2">Authors from Host Country</th>
+                                        <th className="border-2 p-2">International Authors</th>
                                     </tr>
                                     <tr>
-                                        <td className="border-2 border-black p-2">Early Registration</td>
-                                        <td className="border-2 border-black p-2">INR 3,000</td>
-                                        <td className="border-2 border-black p-2">USD 125</td>
+                                        <td className="border-2 p-2 text-center">INR 3,000</td>
+                                        <td className="border-2 p-2 text-center">USD 125</td>
                                     </tr>
                                     <tr>
-                                        <td className="border-2 border-black p-3" colSpan="3">
-                                            This Registration fee is applicable to the second registration of any co-author of an already 
-                                            registered paper. If the co-authors of a paper are looking for additional registration, the above fees are 
-                                            applicable. This fee is also applicable to Attendees, Listeners, or Spouses who wish to participate in the 
-                                            conference and discussions. <br/>
-                                            Participating Co-Author will get the certificate of participation
+                                        <td className="border-2 p-3" colSpan="3">
+                                        All the registered Co-Author or Attendee or Listener or Spouse is eligible to get the certificate of participation (subject to condition that if they have attended conference in physical or online mode).
                                         </td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
-                    {/* task to do */}
+                    {/* task to do
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20 mt-7">
-                        <div><strong>Registration Fees - Co-Author or Attendee/Listener or Spouse who have not attended conference physically</strong><br />
+                        <div><strong className="text-xl">Registration Fees - Co-Author or Attendee/Listener or Spouse who have not attended conference physically</strong><br />
 
                         <table className="border-2 border-black my-2 w-full">
                                 <tbody>
@@ -122,19 +116,17 @@ export default function Registration() {
                                 </tbody>
                             </table>
                         </div>
-                    </div>
+                    </div> */}
 
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <p><strong>Payment option :  <a href="/">Payment Link</a><br />
+                        <div className="text-xl">Payment option : </div><a href="/"className=" underline">Payment Link</a><br />
                             Payment Mode: Mention your Paper ID with Conference Acronym or the Registered Author Name at the payment subject
-                        </strong>
-                        </p>
                     </div>
                     <br />
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <p><strong>Step 2 - Registration</strong><br />
+                        <p><strong className="text-xl">Step 2 - Registration</strong><br />
                             It is mandatory for at least one author of an accepted paper to register in order for the paper to appear in the Proceedings and be included in the Technical Program. If an author has got more than one accepted papers, each paper has to be registered seperately. <br /> <br />
                             <b>The papers of quality are assigned to LONG, FULL and REGULAR paper categories  by the reviewers based on the International standards in vogue and also based on the extent of the content revealed. NO change in category is allowed and possible.</b> <br /><br />
                             The Conference Registration Form available at  DOC (link to form ehich will be provided)
@@ -144,54 +136,28 @@ export default function Registration() {
                     <br />
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <p><strong>Step 3 - Copyright Transfer</strong><br />
-                            All Authors have to sign (for their individual paper) <b>Copyright Transfer Agreement Form</b>, which is in PDF format. Print-Fill-Sign-Scan and send to <b>xyz@gmail.com</b><br />
-
-                            The Conference Copyright form, available at <a href="/" className="text-blue-900 underline font-bold ">DOC</a>.
+                        <p><strong className="text-xl">Step 3 - Copyright Transfer</strong><br />
+                            All Authors have to sign (for their individual paper) Copyright Transfer Agreement Form, which is in PDF format. Print-Fill-Sign-Scan and send to <b>xyz@gmail.com</b><br />
+                            The Conference Copyright form, available at <a href="/" className=" underline font-bold ">DOC</a>.
                         </p>
                     </div>
                     <br /><br />
 
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
                         <div>
-                            <strong>Step 4 - Camera Ready Paper (CRP) Submission</strong><br />
-                            Authors submitting their papers may use the following template. The failure in adhering to the specific format norms  will result in rejection. All camera ready papers must be submitted in MS WORD file format only (PDF is NOT accepted).<br />
-
-                            The GRENZE common template, available at <a href="/documents/grenze_template.docx" className="text-blue-900 underline font-bold ">DOC</a>.  <br />
-                        <p>Maximum allowed and Minimum required pages (while submitting the camera ready paper) for different categories are limited to as follows</p>
+                            <strong className="text-xl">Step 4 - Camera Ready Paper (CRP) Submission</strong><br />
+                            Authors submitting their papers may use the following template. The failure in adhering to the specific format norms  will result in rejection. All camera ready papers must be submitted in MS WORD file format only <b>(PDF is NOT accepted)</b>.<br/>
+                            <div className="mt-2">The CAMERA-READY PAPER common TEMPLATE, available at <a href="https://www.ycce-act.com/documents/grenze_template.docx" className=" font-bold underline">DOC</a>. </div>
+                            <div className="mt-3 font-bold">Submit your camera-ready papers through the google form (Link will be communicated after acceptance of paper).
+                            </div>
                         </div>
-
-                        <table className="border-2 border-black my-2 w-full">
-                                <tbody>
-                                    <tr>
-                                        <th className="border-2 border-black p-2">Paper Category</th>
-                                        <th className="border-2 border-black p-2">Free Pages</th>
-                                        <th className="border-2 border-black p-2">Paying Extra Charge</th>
-                                    </tr>
-                                    <tr>
-                                        <td className="border-2 border-black p-2">Long paper</td>
-                                        <td className="border-2 border-black p-2">09 Pages</td>
-                                        <td className="border-2 border-black p-2">3 Pages</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border-2 border-black p-2">Full Paper</td>
-                                        <td className="border-2 border-black p-2">07 Pages</td>
-                                        <td className="border-2 border-black p-2">2 Pages</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="border-2 border-black p-2">Regular Paper</td>
-                                        <td className="border-2 border-black p-2">05 Pages</td>
-                                        <td className="border-2 border-black p-2">1 Pages</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <p>Send your camera ready papers to only <b>act_phase1outlook.com</b></p>
+              
                     </div>
                     <br />
                     <br />
                     <div className="text-justify leading-4 md:leading-5 lg:leading-normal text-sm md:text-md lg:text-lg lg:px-20">
-                        <p><strong>Step 5 - Registration</strong> <br />
-                        Send your completed registration and copyright form, camera ready paper in word format along with the scanned copy of the proof of payments to <b>act_phase1outlook.com</b>.
+                        <p><strong className="text-xl">Step 5 - Registration</strong> <br />
+                        Send your completed registration and copyright form, camera-ready paper in word format along with the scanned copy of the proof of payments<b> through the google form (Link will be communicated after acceptance of paper).</b>
                         </p>
                     </div>
                     
